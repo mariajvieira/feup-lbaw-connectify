b66cd82d199b95784796ee966264f19f42e5adab
Duarte Marques <up202204973@up.pt>
2024-12-22 14:46:56 +0000

    Delete Account update

--- resources/views/pages/user.blade.php
+++ resources/views/pages/user.blade.php
@@ -13,47 +13,6 @@
         </div>
     </div>
 @else
-<!-- Botão para Excluir Conta -->
-<!-- Caso o usuário seja um administrador, ele verá a opção de excluir a conta de outro usuário -->
-{{-- Exibe o perfil do usuário --}}
-@if($user->id == Auth::id() || Auth::user()->isAdmin())
-    {{-- O botão de exclusão será visível para o próprio usuário ou para um administrador --}}
-    <form action="{{ route('delete.account', ['userId' => $user->id]) }}" method="POST">
-        @csrf
-        @method('DELETE')
-        <button type="submit" onclick="return confirm('Tem certeza de que deseja excluir esta conta?')">Excluir Conta</button>
-    </form>
-@endif
-
-
-
-<!-- Modal de Confirmação 
-<div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="confirmDeleteModalLabel">Tem certeza?</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar" id="closeModalBtn">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                Você tem certeza que deseja apagar a sua conta? Essa ação não pode ser desfeita.
-            </div>
-            <div class="modal-footer">
-                
-                <button type="button" class="btn btn-secondary" id="cancelBtn">Cancelar</button>
-                
-                <form action="{{ route('delete.account') }}" method="POST" id="deleteAccountForm">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger">Sim, apagar conta</button>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
--->
 
 
 
@@ -153,6 +112,51 @@
         </div>
     @endif
 
+<!-- Botão para Excluir Conta -->
+<!-- Caso o usuário seja um administrador, ele verá a opção de excluir a conta de outro usuário -->
+{{-- Exibe o perfil do usuário --}}
+@if($user->id == Auth::id() || Auth::user()->isAdmin())
+    {{-- O botão de exclusão será visível para o próprio usuário ou para um administrador --}}
+    <div class="d-flex justify-content-center mt-3">
+        <form action="{{ route('delete.account', ['userId' => $user->id]) }}" method="POST">
+            @csrf
+            @method('DELETE')
+            <button type="submit" onclick="return confirm('Tem certeza de que deseja excluir esta conta?')" class="btn btn-danger">
+                Excluir Conta
+            </button>
+        </form>
+    </div>
+@endif
+
+
+
+<!-- Modal de Confirmação 
+<div class="modal fade" id="confirmDeleteModal" tabindex="-1" role="dialog" aria-labelledby="confirmDeleteModalLabel" aria-hidden="true">
+    <div class="modal-dialog" role="document">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="confirmDeleteModalLabel">Tem certeza?</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Fechar" id="closeModalBtn">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                Você tem certeza que deseja apagar a sua conta? Essa ação não pode ser desfeita.
+            </div>
+            <div class="modal-footer">
+                
+                <button type="button" class="btn btn-secondary" id="cancelBtn">Cancelar</button>
+                
+                <form action="{{ route('delete.account') }}" method="POST" id="deleteAccountForm">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger">Sim, apagar conta</button>
+                </form>
+            </div>
+        </div>
+    </div>
+</div>
+-->
 
 
     <div class="row mt-4">
