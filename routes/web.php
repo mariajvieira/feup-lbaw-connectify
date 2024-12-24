e0756188eb603b4f280ddb39e91fd23876cbbfbc
Duarte Marques <up202204973@up.pt>
2024-12-24 07:54:00 +0000

    Routes

--- routes/web.php
+++ routes/web.php
@@ -171,7 +171,7 @@ Route::controller(GoogleController::class)->group(function () {
 
 // Delete user
 Route::delete('/delete-account/{userId?}', [DeleteAccountController::class, 'deleteAccount'])->name('delete.account');
-Route::get('/group/{groupId}/members', [GroupController::class, 'viewMembers'])->name('group.members');
+Route::get('/group/{groupId}/members', [GroupController::class, 'viewMembers'])->name('group.members.view');
 Route::get('/groups/{groupId}/leave', [GroupController::class, 'leaveGroup'])->name('group.leave');
 
 //Remove group member (owner)
